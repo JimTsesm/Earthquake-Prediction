6cb36cc2f5d7f9986ef78ff0c904f117fb1e8ca3
JimTsesm <zas1122@hotmail.com>
2019-02-04 11:52:12 +0200

    Updated
    !!! This code requires to run download_dataset.py code !!!
    After executing the commands the file first_two_earthquakes.csv will be created to datasets/Eartquake_prediction/ (Google Drive)

--- src/utilities/Get_first_N_Earthquakes.py
+++ src/utilities/Get_first_N_Earthquakes.py
@@ -3,6 +3,22 @@
 from google.colab import drive
 drive.mount('/content/gdrive')
 
-#Use the indexes to define which dataset you will save
-
+#Use the indexes to define which dataset you will save.
+#Choose an index and copy-paste the number after "-n"
+#Earthquake 1: 5656574 
+#Earthquake 2:50085877
+#Earthquake 3:104677355
+#Earthquake 4:138772452
+#Earthquake 5:187641819
+#Earthquake 6:218652629
+#Earthquake 7:245829584
+#Earthquake 8:307838916
+#Earthquake 9:338276286
+#Earthquake 10:375377847
+#Earthquake 11:419368879
+#Earthquake 12:461811622
+#Earthquake 13:495800224
+#Earthquake 14:528777114
+#Earthquake 15:585568143
+#Earthquake 16:621985672
 !head -n 50085877 /content/data/earthquake/train.csv > /content/gdrive/My\ Drive/datasets/Eartquake_prediction/first_two_earthquakes.csv
