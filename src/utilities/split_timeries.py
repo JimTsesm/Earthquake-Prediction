0e00278f21c4a6120505225663772f4fa2fbb9a6
Dimitrios Tsesmelis <dimitrios.tsesmelis@accenture.com>
2019-02-04 15:21:40 +0200

    Create function get_sub_time_serie() to split timeseries with step as parameter

--- src/utilities/split_timeries.py
+++ src/utilities/split_timeries.py
@@ -1,5 +1,7 @@
 import math 
 import csv
+import numpy as np
+from numpy import genfromtxt
 
 def get_sub_time_serie(start, end):
 	list_of_time_series = []
@@ -22,20 +24,35 @@ def get_sub_time_serie(start, end):
 				break
 	return np.array(time_to_failure_list), np.array(list_of_time_series)
 	
-
-NROWS = 100000000
-START = 5656574
-END = 50085877
-#END = 6000000
+def get_sub_time_serie(dataset, dateset_len, window_size, step):
+  start = 1
+  end = start+150000
+  list_of_time_series = []
+  time_to_failure_list = []
+  while(end <= dateset_len):
+    print(str(start)+' '+str(end))
+    time_to_failure_list.append(dataset[end-1][1])
+    list_of_time_series.append(dataset[start:end, 0])
+    start += step
+    end += step
+  return np.array(time_to_failure_list), np.array(list_of_time_series)
+	
+DATASET_FILE_PATH = '/content/gdrive/My Drive/datasets/Eartquake_prediction/small.csv'
+STEPS = 5000
 TIME_SERIES_LENGTH = 150000
+#NROWS = 100000000
+#START = 5656574
+#END = 50085877
 
 datapoints_sum = END - START
 time_series_number = math.floor(datapoints_sum/TIME_SERIES_LENGTH)
 print(time_series_number)
 
+#Read dataset to numpy array
+#dataset = genfromtxt(DATASET_FILE_PATH, delimiter = ',')
+labels, sub_timeseries = get_sub_time_serie(dataset[0:1000001], 1000001, TIME_SERIES_LENGTH, STEPS)
+print(len(labels))
 
-labels, time_series = 	get_sub_time_serie(START, END)
-print(np.shape(labels))
-print(np.shape(time_series))
+
 
 	
