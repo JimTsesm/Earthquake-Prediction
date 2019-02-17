7de9312318d00317f9ceef0c643955db43adc865
JimTsesm <zas1122@hotmail.com>
2019-02-17 19:50:46 +0200

    Add files via upload

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 def normalize_dataset():
 	dt = { 'acoustic_data': 'i2', 'time_to_failure': 'f8' }
@@ -13,5 +14,8 @@ def normalize_dataset():
 	write_dataset(data_scaled,time_to_failure,'/content/gdrive/My Drive/NormalizedDataset/','NormalizedDataset.csv')
 
 def normalize_dataset(train_inputs):
+	min_max_scaler = MinMaxScaler(feature_range=(0, 1))
 	data_scaled = min_max_scaler.fit_transform(train_inputs)
 	return data_scaled
+
+normalize_dataset([[1,2],[3,4]])
